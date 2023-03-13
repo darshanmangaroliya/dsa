b3100bae9f5ea9c6df26acb78460f7a829798827
darshanmangaroliya <darshanmangaroliya1710@gmail.com>
2023-03-13 16:09:38 +0530

    change typo

--- Array/RotateImage.ts
+++ Array/RotateImage.ts
@@ -3,20 +3,20 @@
  */
  function rotate(matrix: number[][]): void {
    
-    const transformmatrix =()=>{
-       for (let i = 0; i < matrix.length; i++) {
-        for (let j = 0; j < matrix.length; j++) {
-            [matrix[i][j],matrix[j][i]] =[matrix[j][i],matrix[i][j]]
-        }
+   const transformmatrix =()=>{
+      for (let i = 0; i < matrix.length; i++) {
+       for (let j = i+1; j < matrix.length; j++) {
+           [matrix[i][j],matrix[j][i]] =[matrix[j][i],matrix[i][j]]
        }
-    }
-    const rotateIndex =()=>{
-       for (let i = 0; i < matrix.length; i++) {
-        for (let j = 0; j < matrix.length; j++) {
-            [matrix[i][j],matrix[i][matrix.length-1-j]] = [matrix[i][matrix.length-1-j],matrix[i][j],] 
-        }
+      }
+   }
+   const rotateIndex =()=>{
+      for (let i = 0; i < matrix.length; i++) {
+       for (let j = 0; j < matrix.length/2; j++) {
+           [matrix[i][j],matrix[i][matrix.length-1-j]] = [matrix[i][matrix.length-1-j],matrix[i][j]] 
        }
-    }
-    transformmatrix()
-    rotateIndex()
+      }
+   }
+   transformmatrix()
+   rotateIndex()
  };
